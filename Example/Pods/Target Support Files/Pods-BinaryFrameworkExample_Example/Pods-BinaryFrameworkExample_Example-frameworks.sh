f95cc642e2770a6a1bcfe6e16eee85be762dc730
Chittapon Thongchim <chittapon@central.tech>
2021-09-25 21:19:06 +0700

    Update zip file

--- Example/Pods/Target Support Files/Pods-BinaryFrameworkExample_Example/Pods-BinaryFrameworkExample_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-BinaryFrameworkExample_Example/Pods-BinaryFrameworkExample_Example-frameworks.sh
@@ -176,10 +176,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../BinaryFrameworkExample.framework"
+  install_framework "${PODS_ROOT}/GoogleAds-IMA-iOS-SDK/GoogleInteractiveMediaAds.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../BinaryFrameworkExample.framework"
+  install_framework "${PODS_ROOT}/GoogleAds-IMA-iOS-SDK/GoogleInteractiveMediaAds.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
